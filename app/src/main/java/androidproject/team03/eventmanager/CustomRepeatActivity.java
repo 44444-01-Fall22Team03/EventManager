0ee311a9a04a555608a05cab85bd51b30acc8f20
Jaichand Mulakalapalli <mjaichandms@gmail.com>
2022-10-06 10:32:00 -0500

    Initial Commit

---
+++ app/src/main/java/androidproject/team03/eventmanager/CustomRepeatActivity.java
@@ -0,0 +1,13 @@
+package androidproject.team03.eventmanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CustomRepeatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_custom_repeat);
+    }
+}
---
+++ app/src/main/java/androidproject/team03/eventmanager/EventHomeActivity.java
@@ -0,0 +1,14 @@
+package androidproject.team03.eventmanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EventHomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_home);
+    }
+}
