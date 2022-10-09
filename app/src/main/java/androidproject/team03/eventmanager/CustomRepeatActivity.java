3952fa0160b655cd8430406a898773915fa05ad8
manikantaarumilli <98128314+manikantaarumilli@users.noreply.github.com>
2022-10-08 20:59:44 -0500

    completed custom repeat activity layout and added onclick properties to the buttons

--- app/src/main/java/androidproject/team03/eventmanager/CustomRepeatActivity.java
+++ app/src/main/java/androidproject/team03/eventmanager/CustomRepeatActivity.java
@@ -1,13 +1,28 @@
 package androidproject.team03.eventmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class CustomRepeatActivity extends AppCompatActivity {
+
+    private Button repeatCancelBtn, repeatDoneBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_custom_repeat);
+
+        repeatCancelBtn=findViewById(R.id.btnRepeatCancel);
+        repeatDoneBtn=findViewById(R.id.btnRepeatDone);
+
+        repeatCancelBtn.setOnClickListener(v -> {
+            startActivity(new Intent(CustomRepeatActivity.this, CreateEventActivity.class));
+        });
+        repeatDoneBtn.setOnClickListener(v -> {
+            startActivity(new Intent(CustomRepeatActivity.this, CreateEventActivity.class));
+        });
     }
 }
