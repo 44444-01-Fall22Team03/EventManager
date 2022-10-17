a50bf730f8e097416590ff4e16c7ea97cc8459a0
manikantaarumilli <98128314+manikantaarumilli@users.noreply.github.com>
2022-10-16 22:46:39 -0500

    S546660 (Arumilli Manikanta)  : updated custom repeat dropdown and added spinner adapters in java file

--- app/src/main/java/androidproject/team03/eventmanager/CustomRepeatActivity.java
+++ app/src/main/java/androidproject/team03/eventmanager/CustomRepeatActivity.java
@@ -4,7 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CustomRepeatActivity extends AppCompatActivity {
 
@@ -17,6 +24,62 @@ public class CustomRepeatActivity extends AppCompatActivity {
 
         repeatCancelBtn=findViewById(R.id.btnRepeatCancel);
         repeatDoneBtn=findViewById(R.id.btnRepeatDone);
+
+        Spinner spinnerEvery=findViewById(R.id.spinnerEvery);
+        Spinner spinnerRepeatEvery=findViewById(R.id.spinnerRepeatEvery);
+
+        ArrayAdapter<CharSequence> seAdapter=ArrayAdapter.createFromResource(this, R.array.recTypes, android.R.layout.simple_spinner_item);
+
+        seAdapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+
+        spinnerEvery.setAdapter(seAdapter);
+
+        spinnerEvery.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                String option = String.valueOf(spinnerEvery.getSelectedItem());
+
+                if (option.contentEquals("Daily")) {
+                    List<String> list = new ArrayList<>();
+                    list.add("EveryDay");
+                    ArrayAdapter<String> arrayAdapter1 = new ArrayAdapter<>(CustomRepeatActivity.this, android.R.layout.simple_spinner_item, list);
+                    arrayAdapter1.notifyDataSetChanged();
+                    spinnerRepeatEvery.setAdapter(arrayAdapter1);
+                }
+
+                if (option.contentEquals("Weekly")) {
+                    List<String> list = new ArrayList<>();
+                    list.add("EveryWeek");
+                    ArrayAdapter<String> arrayAdapter1 = new ArrayAdapter<>(CustomRepeatActivity.this, android.R.layout.simple_spinner_item, list);
+                    arrayAdapter1.notifyDataSetChanged();
+                    spinnerRepeatEvery.setAdapter(arrayAdapter1);
+                }
+                if (option.contentEquals("Monthly")) {
+                    List<String> list = new ArrayList<>();
+                    list.add("EveryMonth");
+                    list.add("Jan");list.add("Feb");list.add("Mar");
+                    list.add("Apr");list.add("May");list.add("Jun");
+                    list.add("Jul");list.add("Aug");list.add("Sep");
+                    list.add("Oct");list.add("Nov");list.add("Dec");
+                    ArrayAdapter<String> arrayAdapter1 = new ArrayAdapter<>(CustomRepeatActivity.this, android.R.layout.simple_spinner_item, list);
+                    arrayAdapter1.notifyDataSetChanged();
+                    spinnerRepeatEvery.setAdapter(arrayAdapter1);
+                }
+                if (option.contentEquals("Yearly")) {
+                    List<String> list = new ArrayList<>();
+                    list.add("EveryYear");
+                    ArrayAdapter<String> arrayAdapter1 = new ArrayAdapter<>(CustomRepeatActivity.this, android.R.layout.simple_spinner_item, list);
+                    arrayAdapter1.notifyDataSetChanged();
+                    spinnerRepeatEvery.setAdapter(arrayAdapter1);
+                }
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
 
         repeatCancelBtn.setOnClickListener(v -> {
             startActivity(new Intent(CustomRepeatActivity.this, CreateEventActivity.class));
