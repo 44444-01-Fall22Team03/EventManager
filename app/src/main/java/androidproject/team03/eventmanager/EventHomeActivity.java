4ccc6243c149c8fa9e83c4e297339940ad3cbd76
manikantaarumilli <98128314+manikantaarumilli@users.noreply.github.com>
2022-11-18 18:01:23 -0600

    S546660(Arumilli Manikanta): added search by event name functionality in Home Activity

--- app/src/main/java/androidproject/team03/eventmanager/EventHomeActivity.java
+++ app/src/main/java/androidproject/team03/eventmanager/EventHomeActivity.java
@@ -61,6 +61,8 @@ public class EventHomeActivity extends AppCompatActivity {
             String searchText = searchEventET.getText().toString();
             if(searchText.matches("")){
                 Toast.makeText(EventHomeActivity.this, "Search field is Empty.\nPlease input text and try again", Toast.LENGTH_LONG).show();
+            }else{
+                nameSearchDataSetup(searchText);
             }
         });
 
@@ -230,5 +232,57 @@ public class EventHomeActivity extends AppCompatActivity {
                 });
         AlertDialog ok = builder.create();
         ok.show();
+    }
+
+    private void nameSearchDataSetup(String searchText) {
+        myModel1.eventsList.clear();
+
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("Events");
+        Log.v("nameSearchDataSetup: ", "fetching data");
+
+        query.whereContains("eventName", searchText);
+        query.findInBackground(new FindCallback<ParseObject>() {
+            @Override
+            public void done(List<ParseObject> objects, ParseException e) {
+                for (ParseObject event : objects){
+                    if (e == null) {
+                        String event_ID= event.getString("eventID");
+                        String event_Name= event.getString("eventName");
+                        String event_Description= event.getString("eventDescription");
+                        String event_Location= event.getString("eventLocation");
+                        String event_Recurrence=event.getString("eventRecurrence");
+                        String event_Repeat_Every=event.getString("eventRepeatEvery");
+                        boolean is_Sun = event.getBoolean("isSun");
+                        boolean is_Mon= event.getBoolean("isMon");
+                        boolean is_Tues= event.getBoolean("isTues");
+                        boolean is_Wed= event.getBoolean("isWed");
+                        boolean is_Thurs= event.getBoolean("isThurs");
+                        boolean is_Fri= event.getBoolean("isFri");
+                        boolean is_Sat= event.getBoolean("isSat");
+                        String event_End_Dt = String.valueOf(event.getDate("eventEndDt"));
+                        String event_Start_Dt= String.valueOf(event.getDate("eventStartDt"));
+                        String event_Repeat_EndDt= String.valueOf(event.getDate("eventRepeatEndDt"));
+
+                        myModel1.eventsList.add(new EventModel.Events(event_ID, event_Name, event_Start_Dt, event_End_Dt, event_Location, event_Description, event_Repeat_EndDt, event_Recurrence, event_Repeat_Every, is_Sun,  is_Mon,  is_Tues,  is_Wed,  is_Thurs,  is_Fri,  is_Sat));
+
+                        Log.v("description", String.valueOf(event.getString("eventDescription")));
+
+                        Log.v("Size", String.valueOf(myModel1.eventsList.size()));
+
+                    } else {
+                        Toast.makeText(EventHomeActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+
+                    eventRecycler = findViewById(R.id.mRecyclerView);
+
+
+                    eventServer = new EventRAdapter(EventHomeActivity.this, myModel1);
+
+                    Log.v("ItemCount", String.valueOf(eventServer.getItemCount())  );
+                    eventRecycler.setAdapter(eventServer);
+                    eventRecycler.setLayoutManager(new LinearLayoutManager(EventHomeActivity.this));
+                }
+            }
+        });
     }
 }
